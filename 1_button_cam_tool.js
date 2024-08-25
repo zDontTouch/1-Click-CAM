2f51c751b6105023552b03e0c06d812889ca0c78
Leonardo Felini <leonardofelini9857@gmail.com>
2024-08-25 15:28:12 -0300

    Fixed bug when changing case tabs + fixed bug when opening KBA or action plan

--- 1_button_cam_tool.js
+++ 1_button_cam_tool.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     1-Click CAM Tool
-// @version  1.1
+// @version  1.1.1
 // @grant    none
 // @match    *://itsm.services.sap/*
 // @include  *://itsm.services.sap/*
@@ -294,6 +294,9 @@ navigation.addEventListener("navigate", e => {
     document.body.appendChild(camButton);
   }else if(caseData.types[0] == "nocase"){
     document.body.removeChild(document.getElementById("camButton"));
+  }else if(e.destination.url.toString().indexOf("kb_template_kcs_article")>=0 || e.destination.url.toString().indexOf("sn_customerservice_action_plans")>=0){
+    camButton.setAttribute("style","cursor:pointer; z-index:99; display:block; position:absolute; right:162px; top:196px; vertical-align:middle; padding:5.5px 12px; background-color:RGB(var(--now-button--secondary--background-color--hover,var(--now-color--neutral-3,209,214,214)),var(--now-button--secondary--background-color-alpha--hover,var(--now-button--secondary--background-color-alpha,1))); border-color:RGB(var(--now-button--secondary--border-color,var(--now-color--neutral-7,135,147,148))); border-radius:var(--now-button--secondary--border-radius,var(--now-button--border-radius,var(--now-actionable--border-radius,0))); border-width:var(--now-button--secondary--border-width,var(--now-button--border-width,var(--now-actionable--border-width,1px))); color:RGB(var(--now-button--secondary--color,var(--now-color--neutral-18,22,27,28))); font-family: var(--now-button--font-family,var(--now-actionable--font-family,var(--now-font-family,\"Source Sans Pro\",Arial,sans-serif))); font-style:var(--now-button--font-style,var(--now-actionable--font-style,normal)); font-weight:var(--now-button--font-weight,var(--now-actionable--font-weight,normal)); font-size:1rem; line-weight:1.25;");
+    document.body.removeChild(document.getElementById("camButton"));
   }else{
     document.body.appendChild(camButton);
   }
@@ -316,5 +319,4 @@ ise.case.onUpdate2(
         }
       }      
   },
-  //this seems to be what requests communication data from case
   ["headers"]);
